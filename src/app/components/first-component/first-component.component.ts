c262313aa5227752057cef36acf6a58f6c6d110a
tnsilva <tnsilva175@gmail.com>
2023-01-27 19:57:39 -0300

    feat: data interpolations

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Thalisson';
+  age: number = 26;
+  job = 'Programador';
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+  car = {
+    name: 'Polo',
+    year: 2019,
+  };
 }
